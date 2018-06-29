df5175e252f5e87b9762b99e986ffe002cbbe0b0
lixiangsheng <m18826333642@163.com>
2018-06-29 20:10:32 +0800

    2018.6.29

---
+++ src2018.6.29/components/commInfo/inputs.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <span class="list_name">{{item.name}}:</span>
+        <el-input style="width:200px"
+                  :type="item.type"
+                  :placeholder="'请填写'+item.name"
+                  v-model.trim="item.value">
+        </el-input>
+      <span v-show="item.isBool" style="color: red">{{item.isBool}}</span>
+
+    </div>
+</template>
+<script>
+    export default {
+      props:["item"],
+        data() {
+            return {
+
+            }
+        },
+        methods: {
+
+        },
+        created() {
+        }
+    }
+</script>
+<style scoped lang="scss">
+    .list_name{
+      width:93px;
+    }
+</style>
+
+
